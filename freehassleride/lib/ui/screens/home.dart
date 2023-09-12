5a78659f76cffe1db38c0af2f49eceb72240bfd2
WilfredTagha <wilfred.tagha@sumelongenterprice.com>
2023-09-12 10:24:10 +0100

    added the map

--- freehassleride/lib/ui/screens/home.dart
+++ freehassleride/lib/ui/screens/home.dart
@@ -18,62 +18,59 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     var h = MediaQuery.of(context).size.height;
     var w = MediaQuery.of(context).size.width;
-    return const SafeArea(
+    return SafeArea(
         child: Scaffold(
-      body: GoogleMap(
-          initialCameraPosition: CameraPosition(target: currentLocation)),
-
-      //  Container(
-      //   color: Color(0xFFD9D9D9),
-      //   height: h,
-      //   width: w,
-      //   child: Stack(
-      //     children: [
-      //       const GoogleMap(
-      //           initialCameraPosition: CameraPosition(target: LatLng(0, 0))),
-      //       Positioned(
-      //         top: 50,
-      //         right: 15,
-      //         child: Column(
-      //           children: [
-      //             Container(
-      //               decoration: BoxDecoration(
-      //                   color: Color(0xFF19182C),
-      //                   borderRadius: BorderRadius.circular(25)),
-      //               height: 50,
-      //               width: 50,
-      //             ),
-      //             SizedBox(
-      //               height: 10,
-      //             ),
-      //             Container(
-      //               decoration: BoxDecoration(
-      //                   color: Color(0xFF19182C),
-      //                   borderRadius: BorderRadius.circular(25)),
-      //               height: 50,
-      //               width: 50,
-      //               child: Center(
-      //                   child: Text(
-      //                 "N",
-      //                 style: TextStyle(color: Colors.white, fontSize: 20),
-      //               )),
-      //             ),
-      //           ],
-      //         ),
-      //       ),
-      //       Positioned(
-      //           bottom: 20,
-      //           right: 50,
-      //           child: Container(
-      //             width: MediaQuery.of(context).size.width * 0.75,
-      //             decoration: BoxDecoration(
-      //                 color: Color(0xFF19182C),
-      //                 borderRadius: BorderRadius.circular(10)),
-      //             height: 50,
-      //           ))
-      //     ],
-      //   ),
-      // ),
+      body: Container(
+        color: Color(0xFFD9D9D9),
+        height: h,
+        width: w,
+        child: Stack(
+          children: [
+            const GoogleMap(
+                initialCameraPosition: CameraPosition(target: LatLng(0, 0))),
+            Positioned(
+              top: 50,
+              right: 15,
+              child: Column(
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                        color: Color(0xFF19182C),
+                        borderRadius: BorderRadius.circular(25)),
+                    height: 50,
+                    width: 50,
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                        color: Color(0xFF19182C),
+                        borderRadius: BorderRadius.circular(25)),
+                    height: 50,
+                    width: 50,
+                    child: Center(
+                        child: Text(
+                      "N",
+                      style: TextStyle(color: Colors.white, fontSize: 20),
+                    )),
+                  ),
+                ],
+              ),
+            ),
+            Positioned(
+                bottom: 20,
+                right: 50,
+                child: Container(
+                  width: MediaQuery.of(context).size.width * 0.75,
+                  decoration: BoxDecoration(
+                      color: Color(0xFF19182C),
+                      borderRadius: BorderRadius.circular(10)),
+                  height: 50,
+                ))
+          ],
+        ),
+      ),
     ));
   }
 }
